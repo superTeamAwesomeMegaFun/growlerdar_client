86916dfef03ab13e35194e43d04b5ba60e5c40c9
toastynerd <tylermorgan86@gmail.com>
2015-07-18 14:35:27 -0700

    cleaning up modular patterns

--- src/js/components/app.jsx
+++ src/js/components/app.jsx
@@ -1,5 +1,5 @@
 var React = require('react');
-var FilterableDataTable = require('./data-table.jsx');
+var FilterableDataTable = require('./data-table.jsx').FilterableDataTable;
 
 var App = module.exports = exports = React.createClass({
   render: function() {
